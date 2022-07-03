f883c5b51473eeb7f7c32fa9684e853553aa2ba1
MajedALRamdan <majooddark237514@gmail.com>
2022-07-03 20:03:43 +0300

    1st python

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,19 @@
+
+
 def main():
 	# write your code here
 
-
+	num = input('Enter a number from 1 to 12: ')
+	noun = input('Enter a noun (plural): ')
+	name= input('Enter a name: ')
+	sentence = input('Enter any sentence: ')
+	verb = input('Enter a verb: ')
+	print("The Story goes...\n")
+	print("It was "+ num+" o'clock when I heard a knock at the door.")
+	print("I opened the door and there was a box full of "+noun+" with a note saying "+"\"From Mr. "+name.capitalize()+".\"")
+	print("Just as I closed the door I heard a \""+sentence.upper()+".\"")
+	print("I froze in place and all I could do was "+verb+".")
+  
 
 if __name__ == '__main__':
 	main()
